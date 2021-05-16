7d0114a7d23904a49bbde78d78829f6f25573cf4
baracat <joaodobingo@gmail.com>
2021-05-16 08:10:18 -0300

    Add project files.

---
+++ ConsoleApp/src/RabbitMQConsoleApp/RabbitMQConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RabbitMQConsoleApp
+{
+    class Program
+    {
+        private const string password = "guest";
+        private const string userName = "guest";
+        private const string hostName = "localhost";
+
+
+        static void Main(string[] args)
+        {
+
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory() { 
+                Password = password,
+                UserName = userName,
+                HostName = hostName,
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            //model.    methods to comunicate with the queue
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
