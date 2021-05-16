768b7fe1b536d5a7fb84a8d0daa2399852207b0a
baracat <joaodobingo@gmail.com>
2021-05-16 16:21:23 -0300

    feat: docs from instructor

--- ConsoleApp/src/RabbitMQConsoleApp/RabbitMQConsoleApp/Program.cs
+++ ConsoleApp/src/RabbitMQConsoleApp/RabbitMQConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 
 namespace RabbitMQConsoleApp
 {
@@ -21,6 +22,14 @@ namespace RabbitMQConsoleApp
             var connection = connectionFactory.CreateConnection();
             var model = connection.CreateModel();
             //model.    methods to comunicate with the queue
+            model.QueueDeclare("MyQueueConsoleApp", true, false, false, null);
+            Console.WriteLine("Queue created");
+
+            model.ExchangeDeclare("MyExchangeConsoleApp", ExchangeType.Topic);
+            Console.WriteLine("Exchange created");
+
+            model.QueueBind("MyQueueConsoleApp", "MyExchangeConsoleApp", "cars");
+            Console.WriteLine("Exchange and queue bound");
 
             Console.WriteLine("Hello World!");
         }
